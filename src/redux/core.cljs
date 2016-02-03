44af776359346c61c9a1f2dc2e3c34db1c149112
Jelle Akkerman <jelle@wrte.io>
2016-02-03 11:31:05 +0100

    try with multimethods

--- src/redux/core.cljs
+++ src/redux/core.cljs
@@ -13,44 +13,38 @@
 (defn dispatch! [action]
   (swap! !actions conj action))
 
-(defprotocol Action
-  (reducer [action state]))
+(defmulti Action (fn [action state] (:type action)))
 
 (add-watch !actions :reduce
   (fn [_ _ _ new-state]
     (let [new-action (last new-state)]
-      (swap! !state #(reducer new-action %)))))
+      (swap! !state #(Action new-action %)))))
 
-(defn rereduce []
-  (reset! !state (reduce #(reducer %2 %1) initial-state @!actions)))
+(defn rereduce
+  "Reduce past actions over state to get a fresh copy. As all actions
+  are stored as data, changes in actions will be projected on state."
+  []
+  (reset! !state (reduce #(Action %2 %1) initial-state @!actions)))
 
 ;; Making an Action
 
-(defrecord Change [])
-(defrecord Increment [num])
+(defmethod Action :Change [_ state]
+  (update state :list #(conj % "more")))
 
-(extend-protocol Action
-  Change
-  (reducer [_ state]
-    (update state :list #(conj % "more")))
+(defmethod Action :Increment [{:keys [num]} state]
+  (update state :counter #(- % num)))
 
-  Increment
-  (reducer [{:keys [num]} state]
-    (update state :counter #(+ % num))))
 
 (defn root []
   [:div
    [:pre "state: " (str @!state)]
    [:pre "actions: " (str @!actions)]
-   [:button {:on-click #(dispatch! (->Change))} "more"]
-   [:button {:on-click #(dispatch! (->Increment 10))} "+10"]])
+   [:button {:on-click #(dispatch! {:type :Change})} "more"]
+   [:button {:on-click #(dispatch! {:type :Increment :num 10})} "+10"]])
 
 (r/render [root] (js/document.getElementById "app"))
 
-
-(defn on-js-reload []
-  (rereduce)
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn on-js-reload
+  "Hook for figwheel reload"
+  []
+  (rereduce))
